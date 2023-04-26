6921dfd9cee762b034dab4c2ad9cc31c1e405398
VinceGsm <v.gusmini@outlook.fr>
2023-04-26 16:20:46 +0200

    init v1

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Choose an option from the following list:");
+        Console.WriteLine("\t1 - SRT - One Piece");
+        Console.WriteLine("\t2 - SRT - Autre");
+        Console.Write("Your option? ");
+
+        switch (Console.ReadLine())
+        {
+            case "1":
+                Console.WriteLine("Type inputDirectory for selecting mkv :");
+                string inputDirectory = Console.ReadLine();
+                OnePieceAmb3rSrt(inputDirectory);
+                break;
+            case "2":
+                Console.WriteLine("A venir");
+                break;
+        }
+        Console.WriteLine("------------------------\n");
+        Console.WriteLine("All done!");
+        Console.ReadLine();
+    }
+
+    private static void OnePieceAmb3rSrt(string inputDirectory)
+    {
+        string[] mkvFiles = Directory.GetFiles(inputDirectory, "*.mkv");
+
+        foreach (string mkvFile in mkvFiles)
+        {
+            // Build the FFmpeg command to extract the subtitles track
+            // ffmpeg -i Movie.mkv -map 0:s:0 subs.srt
+            string ffmpegCommand = $"-i {mkvFile} -map 0:s:0 {inputDirectory}\\{mkvFile.Substring(28, 14)}.srt";
+
+            // Run the FFmpeg command
+            ProcessStartInfo psi = new ProcessStartInfo("ffmpeg", ffmpegCommand);
+            psi.UseShellExecute = false;
+            psi.RedirectStandardOutput = true;
+            Process process = Process.Start(psi);
+            process.WaitForExit();
+        }
+    }
+}
